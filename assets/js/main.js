1a7849f8a55e78a6506c6d1d7806b36b8e170822
KukuRos <micaela.m.rosa@gmail.com>
2022-03-22 22:44:30 -0300

    Responsive design

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+// ================== SHOW MENU =================== 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+
+    // validate that variables exist
+    if (toggle && nav) {
+        toggle.addEventListener('click', ()=>{
+            // We add the show-menu class to the div tag with the nav__menu class
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
+
+// ================== REMOVE MENU MOBILE ===================
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    // When we clack on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// ================== SCROLL SECTIONS ACTIVE LINK =================== 
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+// ================== SHOW SCROLL TOP =================== 
+function scrollTop() {
+    const scrollTop = document.getElementById('scroll-top');
+    // When the scroll is higher than 200 viewport height, add the show-scroll class to the tag
+    if(this.scrollY >= 200) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
+
+// ================== DARK LIGHT THEME =================== 
+// ================== REDUCE THE SIZE AND PRINT ON AN A4 SHEET =================== 
+// ================== REMOVE THE SIZE THEN THE CV IS DOWNLOADED =================== 
